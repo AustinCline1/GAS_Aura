eb66ff204eeec4033693644c701e67bed881a80b
Austin Cline <au.cline@hotmail.comgit config --global user.name Austin>
2024-08-28 20:26:01 -0400

    Added Weapon Sockets

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -6,8 +6,13 @@
 // Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
 {
- 	
-	
+ 	PrimaryActorTick.bCanEverTick = false;
+	//Creating a Skeletal Mesh Compenent called Weapon
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	//Attaching the Mesh Component using the socket named WeaponHandSocket on the Skeletal Mesh 
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	//Removing Collision from the weapon
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 
 }
 
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -16,6 +16,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-	
-
+	//Adding Skeletal Mesh Component for a socket
+	UPROPERTY(EditAnywhere, Category= Combat);
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 };
